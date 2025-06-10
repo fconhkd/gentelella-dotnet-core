4580b8489c9260775e104da861ea309a1db826f6
FabianoHKD <fabianoconrado@gmail.com>
2025-06-09 23:44:26 -0300

    Update for dotnet 8

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace Gentelella2.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
